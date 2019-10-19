a5c0b9ffafd4a5d993a4727cf67fec039ffec797
tyzo4ka <tyzo4ka@gmail.com>
2019-10-19 18:46:31 +0600

    created statistics.html

--- source/webapp/views/answer.py
+++ source/webapp/views/answer.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.views.generic import TemplateView
-from webapp.forms import PollForm, ChoiceForm
 from webapp.models import Answer, Poll, Choice
 
 
@@ -24,46 +22,3 @@ class AnswerIndexView(View):
         answer.choice = choice
         answer.save()
         return redirect('index')
-
-
-#
-#
-# class AnswerView(View):
-#     def get(self, request, *args, **kwargs):
-#         poll = self.get_object()
-#         return render(request, 'answer/answers.html', {'poll': poll})
-#
-#     def get_object(self):
-#         poll_pk = self.kwargs.get('pk')
-#         return get_object_or_404(Poll, pk=poll_pk)
-#
-#     def post(self, request, *args, **kwargs):
-#         poll_id = kwargs.get('pk')
-#         choice_id = int(request.POST.get('answer'))
-#
-#         pull = get_object_or_404(Poll, pk=poll_id)
-#         choice = get_object_or_404(Choice, pk=choice_id)
-#         answer = Answer()
-#         answer.poll = pull
-#         answer.choice = choice
-#         answer.save()
-#         return redirect('index')
-
-
-# class AnswerCreateView(View):
-#     def get(self, request, *args, **kwargs):
-#         form = ChoiceForm()
-#         return render(request, 'create.html', context={'form': form})
-#
-#     def post(self, request, *args, **kwargs):
-#         form = ArticleForm(data=request.POST)
-#         if form.is_valid():
-#             article = Article.objects.create(
-#                 title=form.cleaned_data['title'],
-#                 author=form.cleaned_data['author'],
-#                 text=form.cleaned_data['text'],
-#                 category=form.cleaned_data['category']
-#             )
-#             return redirect('article_view', pk=article.pk)
-#         else:
-#             return render(request, 'create.html', context={'form': form})
--- source/webapp/views/choice_views.py
+++ source/webapp/views/choice_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from webapp.forms import ChoiceForm, PollChoiceForm
 from webapp.models import Choice, Poll
