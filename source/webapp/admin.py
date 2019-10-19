e3bb80a590d6e315c4adcc3e37df70086f87aae3
tyzo4ka <tyzo4ka@gmail.com>
2019-10-19 13:01:12 +0600

    Changed admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_date']
+    list_filter = ['question']
+    list_display_links = ['pk', 'question']
+    search_fields = ['question']
+    exclude = []
+    readonly_fields = ['created_date']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
